dbe57742bdccf1e24edf200d95b4c3cf87c6bbb7
Mauro Titimoli <mauro@jumpn.com>
2017-10-30 20:36:41 -0300

    chore(src): Autogenerate index using create-index

--- indexIgnore.sed
+++ indexIgnore.sed
@@ -1,1 +1,6 @@
-/.*types\.js.*/d
+/createPushHandler\.js.*$/d
+/handlePush\.js.*$/d
+/joinChannel\.js.*$/d
+/pushRequest\.js.*$/d
+/updateNotifiers\.js.*$/d
+/types\.js.*$/d
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 // @flow
 
-// TODO: use export-default-from once proposal is accepted
-// https://github.com/tc39/proposal-export-default-from
+// @create-index
 
-import create from "./create";
-import discard from "./discard";
-import observe from "./observe";
-import send from "./send";
-import unobserve from "./unobserve";
-
-export {create, discard, observe, send, unobserve};
+export {default as create} from "./create.js";
+export {default as discard} from "./discard.js";
+export {default as observe} from "./observe.js";
+export {default as send} from "./send.js";
+export {default as unobserve} from "./unobserve.js";
